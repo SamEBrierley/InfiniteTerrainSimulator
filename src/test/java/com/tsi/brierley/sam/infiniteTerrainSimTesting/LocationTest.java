f462f046808546db5f6ef0f9f624594e854f4524
SamEBrierley <samellisbrierley@gmail.com>
2022-02-03 14:25:08 +0000

    Added more tests for the location

--- src/test/java/com/tsi/brierley/sam/infiniteTerrainSimTesting/LocationTest.java
+++ src/test/java/com/tsi/brierley/sam/infiniteTerrainSimTesting/LocationTest.java
@@ -6,10 +6,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LocationTest {
 
     @Test
-    public void test_get_xposition(){
+    public void test_get_x_position(){
         Location testLocation = new Location(4,4);
         testLocation.getXPosition();
         assertEquals(4, testLocation.getXPosition(),
+                "The expected x position was wrong");
+    }
+
+    @Test
+    public void test_set_x_position(){
+        Location testLocation = new Location(4,4);
+        testLocation.setXPosition(5);
+        assertEquals(5, testLocation.getXPosition(),
+                "The expected x position was wrong");
+    }
+
+    @Test
+    public void test_get_y_position(){
+        Location testLocation = new Location(4,4);
+        testLocation.getYPosition();
+        assertEquals(4, testLocation.getYPosition(),
+                "The expected x position was wrong");
+    }
+
+    @Test
+    public void test_set_y_position(){
+        Location testLocation = new Location(4,4);
+        testLocation.setYPosition(5);
+        assertEquals(5, testLocation.getYPosition(),
                 "The expected x position was wrong");
     }
 }
