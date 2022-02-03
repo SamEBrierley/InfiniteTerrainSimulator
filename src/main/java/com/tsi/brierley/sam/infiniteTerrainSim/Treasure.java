728d3cd91e1368f881ba901a370ed07117f404a4
SamEBrierley <samellisbrierley@gmail.com>
2022-02-03 11:30:15 +0000

    Added Treasure class and set up testing area

---
+++ src/main/java/com/tsi/brierley/sam/infiniteTerrainSim/Treasure.java
@@ -0,0 +1,4 @@
+package com.tsi.brierley.sam.infiniteTerrainSim;
+
+public class Treasure {
+}
---
+++ src/test/java/com/tsi/brierley/sam/infiniteTerrainSimTesting/TreasureTest.java
@@ -0,0 +1,7 @@
+package com.tsi.brierley.sam.infiniteTerrainSimTesting;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TreasureTest {
+
+}
